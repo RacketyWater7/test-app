e3d8e9bf128f72eb44280f4eddf58acae8215470
Haseeb Udeen <haseebudeen@outlook.com>
2021-12-10 11:41:36 +0530

    rebased

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { MathpixMarkdown, MathpixLoader } from "mathpix-markdown-it";
 import "./App.css";
 
+const styles = {
+  container: (isRowBased) => ({
+    display: "flex",
+    flexDirection: isRowBased ? "row" : "column",
+    justifyContent: "space-around",
+  }),
+};
 function App() {
+  const mediaMatch = window.matchMedia("(min-width: 500px)");
+  const [matches, setMatches] = useState(mediaMatch.matches);
+  useEffect(() => {
+    const handler = (e) => setMatches(e.matches);
+    mediaMatch.addListener(handler);
+    return () => mediaMatch.removeListener(handler);
+  });
   const [val, setVal] = useState(0);
   const statesObject = {
     someState: val,
@@ -10,11 +25,42 @@ function App() {
   const handleClick = () => {
     statesObject.someStateUpdater(val + 1);
   };
+  const thing = `**(3)** This question is about tests for some elements and compounds.
+  **(a)** What is the test for hydrogen?
+ $~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~$**(1)**
+ ____
+ **(b)** The diagram **Fig.6** shows hydrogen burning in air, and how some of the gases passing through the apparatus are collected and tested.
+  A colourless liquid collects in the U-shaped tube and the limewater turns cloudy **very** slowly.
+ 
+ **(i)** Describe a **chemical** test to show that the colourless liquid contains water.
+ $~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~$**(2)**
+ **(ii)** Describe a **physical** test to show that the colourless liquid is pure water.
+ $~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~$**(2)**
+ ___
+ **(iii)** A reaction involving carbon dioxide causes the cloudiness in the limewater.
+  Place crosses (X) in two boxes to show the correct statements about this reaction.
+ - carbon dioxide forms when the hydrogen burns
+ - carbon dioxide from the air reacts to cause the cloudiness
+ - the cloudiness is caused by the formation of calcium hydroxide
+ - the cloudiness is caused by the formation of a white precipitate
+ - the reaction in the limewater is an example of oxidation
+ 
+ $~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~$**(2)**`;
   return (
     <div>
       {statesObject.someState}
       <button onClick={handleClick}>update</button>
-      commit 2 commit 3 commit 4
+      commit 2 commit 3 commit 4 commit 5 commit 6 commit 7 commit 8 commit 9
+      more commits
+      <div style={styles.container(matches)}>
+        <div>First item</div>
+        <div>Second item</div>
+      </div>
+      <div>
+        <MathpixLoader>
+          <MathpixMarkdown text={thing} />
+        </MathpixLoader>
+      </div>
     </div>
   );
 }
