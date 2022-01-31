99df185b601b380829d1b5defbd6f8700e7b58aa
Haseeb Udeen <haseebudeen@outlook.com>
2022-01-31 19:27:19 +0530

    preparing local 2

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
       </div>
       commit 2 commit 3 commit 4 commit 5 commit 6 commit 7 commit 8 commit 9
       blah and blah commit 1000000 more commits preparing ground for rebase from
-      local
+      local 2nd commit for local rebase
     </div>
   );
 }
